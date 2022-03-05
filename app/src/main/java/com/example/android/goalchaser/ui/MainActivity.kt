2c9ef698daf592956b880f69f7a96fde831aa8a5
Gergana Toshkova <geritoshkova85@gmail.com>
2022-03-05 16:41:44 +0200

    add nav drawer navigation and logo and fix some minor ui design flaws

--- app/src/main/java/com/example/android/goalchaser/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/goalchaser/ui/MainActivity.kt
@@ -18,14 +18,20 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.mainAppToolbarIncluded.mainAppToolbar)
-        val drawerLayout: DrawerLayout = binding.mainDrawerLayout
+        setupNavigation()
+
+    }
+
+    private fun setupNavigation() {
+
+        drawerLayout = binding.mainDrawerLayout
         val navView: NavigationView = binding.mainDrawerNavView
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
@@ -36,8 +42,10 @@ class MainActivity : AppCompatActivity() {
                 R.id.activeGoalsFragment, R.id.completedgoalsListFragment, R.id.statisticsFragment
             ), drawerLayout
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        setSupportActionBar(binding.mainAppToolbarIncluded.mainAppToolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
